1b4a659bdbcf32da1c2bef2ea3a0cbd9185bfa00
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-03-17 19:49:08 +0900

    update

--- _next/static/chunks/pages/index-74109990826b0d7a.js
+++ _next/static/chunks/pages/index-74109990826b0d7a.js
@@ -742,7 +742,7 @@
                   (0, R.jsxs)("div", {
                     className: "flex flex-col items-center justify-center mt-8",
                     children: [(0, R.jsxs)("a", {
-                      href: "https://dexscreener.com/solana/0xcomingsoon",
+                      href: "https://dexscreener.com/solana/8cPHPDVthPfkpwo8TbUJAdKVskVbRygPLPfnYQo9pump",
                       children: (0, R.jsxs)("img", {
                         style: { marginBottom: 40 },
                         src: "/aaa.png",
@@ -756,7 +756,7 @@
                             "w-fit p-2 bg-gray-800/50 text-white rounded-lg border border-gray-600",
                           onClick: function () {
                             navigator.clipboard.writeText(
-                              "0xcomingsoon"
+                              "8cPHPDVthPfkpwo8TbUJAdKVskVbRygPLPfnYQo9pump"
                             ),
                               (0, d.h)({
                                 type: "success",
@@ -764,7 +764,7 @@
                               });
                           }, 
                           children:
-                            "CA: 0xcomingsoon",
+                            "CA: 8cPHPDVthPfkpwo8TbUJAdKVskVbRygPLPfnYQo9pump",
                         }),
                       }), 
                       (0, R.jsxs)("div", {
@@ -806,7 +806,7 @@
                             
                           }),
                           (0, R.jsx)("a", {
-                            href: "https://dexscreener.com/solana/0xcomingsoon",
+                            href: "https://dexscreener.com/solana/8cPHPDVthPfkpwo8TbUJAdKVskVbRygPLPfnYQo9pump",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
@@ -819,7 +819,7 @@
                             }),
                           }),
                           (0, R.jsx)("a", {
-                            href: "https://www.dextools.io/app/en/sol/pair-explorer/0xcomingsoon",
+                            href: "https://www.dextools.io/app/en/sol/pair-explorer/8cPHPDVthPfkpwo8TbUJAdKVskVbRygPLPfnYQo9pump",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
