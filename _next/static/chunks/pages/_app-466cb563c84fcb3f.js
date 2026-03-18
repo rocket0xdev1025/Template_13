26bf2d9812c45dd952d094d21ddb216cd621f834
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-03-18 21:17:35 +0900

    update

--- _next/static/chunks/pages/_app-466cb563c84fcb3f.js
+++ _next/static/chunks/pages/_app-466cb563c84fcb3f.js
@@ -21239,7 +21239,7 @@
                         }),
                         (0, We.jsx)("a", {
                           className: "text-[#3ed44f] text-4xl",
-                          href: "https://dexscreener.com/solana/4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump",
+                          href: "https://dexscreener.com/solana/GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump",
                           target: "_blank",
                           children: "Buy Now",
                         }),
--- _next/static/chunks/pages/index-74109990826b0d7a.js
+++ _next/static/chunks/pages/index-74109990826b0d7a.js
@@ -742,7 +742,7 @@
                   (0, R.jsxs)("div", {
                     className: "flex flex-col items-center justify-center mt-8",
                     children: [(0, R.jsxs)("a", {
-                      href: "https://dexscreener.com/solana/4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump",
+                      href: "https://dexscreener.com/solana/GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump",
                       children: (0, R.jsxs)("img", {
                         style: { marginBottom: 40 },
                         src: "/aaa.png",
@@ -756,7 +756,7 @@
                             "w-fit p-2 bg-gray-800/50 text-white rounded-lg border border-gray-600",
                           onClick: function () {
                             navigator.clipboard.writeText(
-                              "4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump"
+                              "GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump"
                             ),
                               (0, d.h)({
                                 type: "success",
@@ -764,7 +764,7 @@
                               });
                           }, 
                           children:
-                            "CA: 4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump",
+                            "CA: GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump",
                         }),
                       }), 
                       (0, R.jsxs)("div", {
@@ -806,7 +806,7 @@
                             
                           }),
                           (0, R.jsx)("a", {
-                            href: "https://dexscreener.com/solana/4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump",
+                            href: "https://dexscreener.com/solana/GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
@@ -819,7 +819,7 @@
                             }),
                           }),
                           (0, R.jsx)("a", {
-                            href: "https://www.dextools.io/app/en/sol/pair-explorer/4UbqXmdfZMtbxBZFH3JDRwqn3xBdEh7sjMZp78Z6pump",
+                            href: "https://www.dextools.io/app/en/sol/pair-explorer/GzTjtCJAEr49BZ7a78XRDMhZ22AGvmp99xMahhNDpump",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
